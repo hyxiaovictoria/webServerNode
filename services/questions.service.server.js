5a5b04ad56e144682280347b021c76d650b52508
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-30 01:32:32 -0700

    Question service controller is done. Plus allow 4200 access.

---
+++ services/questions.service.server.js
@@ -0,0 +1,18 @@
+const questions = require('./questions')
+
+findAllQuestions = () => questions
+
+findQuestionById = (qid) =>
+    questions.find(question =>
+        question._id === qid)
+
+findQuestionsForQuiz = (qzid) =>
+    questions.filter(question =>
+        question.quizId === qzid
+    )
+
+module.exports = {
+    findAllQuestions,
+    findQuestionById,
+    findQuestionsForQuiz
+}
