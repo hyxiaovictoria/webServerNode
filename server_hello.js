1753d76bba498733390ac5c5e907c90e68d74c70
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-04-09 20:56:59 -0700

    Changes to make it work on Heroku.

--- server_hello.js
+++ server_hello.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path')
 var app = express();
+
+app.use(express.static(__dirname + '/dist/wbdv-sp20-xiaohai-server-node'));
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
 app.get('/', function(req, res){
     res.send('Welcome to server_hello.js!!!');
 });
-app.listen(3000);
+app.listen(process.env.PORT||8080);
